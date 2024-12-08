a97d0c919b85562ccb628dcbd99f7446208ed05c
David Hudson <dhudson@dphsw.co.uk>
2024-12-08 09:19:51 +0000

    Start project to create file of graphs

---
+++ TesseractGraphs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using CubeNetsLibrary;
+
+List<CubeNet> FirschingNets = CubeNet.GetTesseractNets();
+
+List<Tree> Trees = FirschingNets.Select(net => net.ToTree()).ToList();
+
+Debug.Assert(Trees.Count == 261);
+
+ConcurrentDictionary<int, float[]> graphs = new ConcurrentDictionary<int, float[]>();
+
+Parallel.ForEach(Enumerable.Range(0, 261), n => {
+    graphs.TryAdd(n, Trees[n].FindGraphCoords());
+});
+
+
